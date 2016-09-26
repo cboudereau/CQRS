f5f3650f8b93a733e4c10871ee91059c731487c7
@cboudereau <cboudereau@gmail.com>
2016-09-26 09:09:30 +0200

    Update SearchBookingProposalQuery.cs

--- src/BookARoom.Domain/ReadModel/SearchBookingProposalQuery.cs
+++ src/BookARoom.Domain/ReadModel/SearchBookingProposalQuery.cs
@@ -4,8 +4,8 @@ namespace BookARoom.Domain.ReadModel
 {
     public class SearchBookingProposalQuery
     {
-        public DateTime CheckInDate { get; }
-        public DateTime CheckOutDate { get; }
+        public DateTime CheckInDate { get; } //Don't forget the timezone (confusion, ...), the timezone will be always for the target location (to the hotel)
+        public DateTime CheckOutDate { get; } // Checkin/ Checkout date are night date, for a stay from 01/01/2016 => 01/01/2016 it is on night. For the algorithm it is better to use Half open interval : 01/01/2016 => 02/01/2016 even if the model impose night count.
         public string Location { get; }
         public int AdultsCount { get; }
         public int NumberOfRoomsNeeded { get; }
