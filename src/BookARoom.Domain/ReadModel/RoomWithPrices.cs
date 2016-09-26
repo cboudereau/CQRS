b074d37569024dfb807f228ec3c94f284711afb8
@cboudereau <cboudereau@gmail.com>
2016-09-26 09:00:00 +0200

    Update RoomWithPrices.cs

--- src/BookARoom.Domain/ReadModel/RoomWithPrices.cs
+++ src/BookARoom.Domain/ReadModel/RoomWithPrices.cs
@@ -2,10 +2,12 @@
 {
     public class RoomWithPrices
     {
-        public string RoomIdentifier;
-        public Price PriceForOneAdult;
-        public Price PriceForTwoAdults;
-
+        public string RoomIdentifier; //In Fact, in business term, this is called the room code. Code identity the kind of a room twin, double single
+        public Price PriceForOneAdult; //This is call OneAdultOccupancy
+        public Price PriceForTwoAdults; //TwoAdultOccupancy
+        
+        //What about Breakfast it is a common use, you can simplify by adding a fixed price for the breakfast? 
+        
         public RoomWithPrices(string roomIdentifier, Price priceForOneAdult, Price priceForTwoAdults)
         {
             RoomIdentifier = roomIdentifier;
